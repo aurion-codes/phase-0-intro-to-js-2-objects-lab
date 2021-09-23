66970d21c6cbcce763a07200c126d6f11f86fdb2
AurionHoward <aurionhoward@gmail.com>
2021-09-23 00:19:35 -0400

    commit tree d711e260f0573d154c3e58cea0d49bfacf92b4f0

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const employee = {
-    name: '',
-    streetAdress:'12 Broadway'
+    name: 'Sam',
+    streetAddress: '11 Broadway'
 }
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObject ={ ...employee}
-    newObject[key] = value
-    return newObject;
+
+       const newObject = {...employee}
+       newObject[key] = value
+       return newObject;
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value
     return employee
 }
+    
 
-function deleteFromEmployeeByKey(employee, key){
-    const newObject =Object.assign({}, employee)
-    delete newObject[key]
-    return newObject
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key]
-    return employee
-
-}
